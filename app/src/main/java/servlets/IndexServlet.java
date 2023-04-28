3bbf164438dfa363af7813baff0d6730aab2831f
grazicristini <grazielicristhyny2002@gmail.com>
2023-04-28 15:03:33 +0000

    salvar

--- app/src/main/java/servlets/IndexServlet.java
+++ app/src/main/java/servlets/IndexServlet.java
@@ -1,5 +1,7 @@
 package servlets;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,7 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "IndexServlet", urlPatterns="/")
 public class IndexServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletsException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().println("Oi Servlet!");
         response.getWriter().close();
     }
